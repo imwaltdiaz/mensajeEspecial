ab82a0ed6ca9f14212383527fc57048eeee74f07
walter <walter@waltdiaz.com>
2022-11-10 21:59:49 -0500

    alert message with love

---
+++ index.js
@@ -0,0 +1,8 @@
+function mensaje(){
+  alert("Hola amor 💕");
+  alert("Quise hacerte un recordatorio rápido");
+  alert("Jugando rápido con desarrollo web")
+  alert("De que te amo mucho Noyra 💕✨");
+  alert("💞💝💘💖");
+}
+mensaje()
